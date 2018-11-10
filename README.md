822eea5b28401f21dd5da590f36d719b8b975d9c
richardpct <noreply>
2018-11-10 22:08:02 +0100

    adding functions

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # macos-libtool
 libtool package for macOS
+
+# Build
+$ go build libtool.go
+
+# Usage
+$ ./libtool -destdir=whereyouwant
+where destdir is the target directory installation
---
+++ libtool.go
@@ -0,0 +1,88 @@
+// libtool package
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/richardpct/pkgsrc"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+)
+
+var destdir = flag.String("destdir", "", "directory installation")
+var pkg pkgsrc.Pkg
+
+const (
+	name     = "libtool"
+	vers     = "2.4.6"
+	ext      = "tar.xz"
+	url      = "http://ftpmirror.gnu.org/libtool"
+	hashType = "sha256"
+	hash     = "7c87a8c2c8c0fc9cd5019e402bed4292462d00a718a7cd5f11218153bf28b26f"
+)
+
+func checkArgs() error {
+	if *destdir == "" {
+		return fmt.Errorf("Argument destdir is missing")
+	}
+	return nil
+}
+
+func configure() {
+	fmt.Println("Waiting while configuring ...")
+	cmd := exec.Command("./configure",
+		"--prefix="+*destdir,
+		"--program-prefix=g")
+	if out, err := cmd.Output(); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf("%s\n", out)
+	}
+}
+
+func build() {
+	fmt.Println("Waiting while compiling ...")
+	cmd := exec.Command("make")
+	if out, err := cmd.Output(); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf("%s\n", out)
+	}
+}
+
+func install() {
+	fmt.Println("Waiting while installing ...")
+	cmd := exec.Command("make", "install")
+	if out, err := cmd.Output(); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf("%s\n", out)
+	}
+}
+
+func main() {
+	flag.Parse()
+	if err := checkArgs(); err != nil {
+		log.Fatal(err)
+	}
+
+	pkg.Init(name, vers, ext, url, hashType, hash)
+	pkg.CleanWorkdir()
+	if !pkg.CheckSum() {
+		pkg.DownloadPkg()
+	}
+	if !pkg.CheckSum() {
+		log.Fatal("Package is corrupted")
+	}
+
+	pkg.Unpack()
+	wdPkgName := path.Join(pkgsrc.Workdir, pkg.PkgName)
+	if err := os.Chdir(wdPkgName); err != nil {
+		log.Fatal(err)
+	}
+	configure()
+	build()
+	install()
+}
